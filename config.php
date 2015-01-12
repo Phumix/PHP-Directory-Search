7fbf609a4377812587ce82ea2d27b8fe4476ab14
Martin <Phumix@users.noreply.github.com>
2015-01-12 13:43:55 +0100

    Create config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+// Configuration file. 
+
+// Directory where the searchable files are located.
+    $dir = 'files/';
+	
+// File extension of the files you wish to display as results.
+    $ext = '.txt';
+	
+?>
